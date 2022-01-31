737a00e70b739ac6a194a3c278378257789d5cb2
Antonio Palmeira <av.palmeira@gmail.com>
2022-01-31 14:21:29 -0300

    WIP: Adjust section layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,8 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.css'
+
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStethoscope, faHeartbeat, faClock, faClinicMedical } from '@fortawesome/free-solid-svg-icons'
+import { faWhatsapp, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import styles from '../styles/Home.module.css'
-
 import heroImg from '../public/big-eye.png'
 import logo from '../public/logo.jpg'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Seu Oftalmo</title>
         <meta
@@ -20,7 +22,10 @@ export default function Home() {
       <main>
         <header className={styles.header}>
           <Image src={logo} width={141} height={65} alt="SeuOftalmo Logo"/>
-          <a href="#" className={styles.actionButton}>Botão Agendar Consulta</a>
+          <a href="#" className={styles.actionButton}>
+            Botão Agendar Consulta
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </a>
         </header>
         <section className={styles.hero}>
           <Image className={styles.heroImg} src={heroImg} alt="Visão melhorada" />
@@ -31,23 +36,37 @@ export default function Home() {
         </section>
         <section className={styles.services}>
           <h2 className={styles.servicesTitle}>Nossos Serviços</h2>
-          <p>Equipe médica com mais de 35 anos de experiência</p>
-          <p>Exames, diagnósticos e procedimentos</p>
-          <p>Agilidade em marcação de horário e atendimento</p>
-          <p>Estrutura e equipamentos modernos</p>
+          <div className={styles.serviceList}>
+            <div className={styles.service}>
+              <FontAwesomeIcon icon={faStethoscope} />
+              <p>Equipe médica com mais de 35 anos de experiência</p>
+            </div>
+            <div className={styles.service}>
+              <FontAwesomeIcon icon={faHeartbeat} />
+              <p>Exames, diagnósticos e procedimentos</p>
+            </div>
+            <div className={styles.service}>
+              <FontAwesomeIcon icon={faClock} />
+              <p>Agilidade em marcação de horário e atendimento</p>
+            </div>
+            <div className={styles.service}>
+              <FontAwesomeIcon icon={faClinicMedical} />
+              <p>Estrutura e equipamentos modernos</p>
+            </div>
+          </div>
         </section>
         <section className={styles.procedures}>
           <h2 className={styles.proceduresTitle}>Exames e Procedimentos</h2>
-          <div>
-            <p>Exames</p>
+          <div className={styles.proceduresList}>
+            <h3>Exames</h3>
             <p>
               O.C.T  •  Pentacam  •  Optomap  Interferometria  •  Yag Laser  •  Curva Tensional  •  Campimetria
               •  Biometria Mapeamento  •  Tonometria  •  Gonioscopia  •  Capsulotomia  •  Retinografia
               •  Ultrassonografia  •  Acuidade Visual  •  Microscopia  Ceratoscopia  •  Paquimetria
             </p>
           </div>
-          <div>
-            <p>Procedimentos</p>
+          <div className={styles.proceduresList}>
+            <h3>Procedimentos</h3>
             <p>Pterígio (carne crescida)  •  Calázio  •  Refrativa  •  Catarata</p>
           </div>
         </section>
@@ -70,6 +89,10 @@ export default function Home() {
           <p>Queremos te ajudar.</p>
         </section>
         <footer className={styles.footer}>
+          <div>
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faInstagram} />
+          </div>
           <p>Copyright 2022 SeuOftalmo. Todos os direitos reservados.</p>
           <p>O site SeuOftalmo.com.br é de propriedade da Visão 360 Ltda. registrada sob o CNPJ 25.186.815/0001-38</p>
         </footer>
