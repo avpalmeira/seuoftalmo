b33b457b92f8cf32192c152ada2b582b3a2a98a3
Antonio Palmeira <av.palmeira@gmail.com>
2022-02-20 15:50:15 -0300

    Selected address logic

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,14 +4,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStethoscope, faHeartbeat, faClock, faClinicMedical } from '@fortawesome/free-solid-svg-icons'
 import { faWhatsapp, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
 
 import heroImg from '../public/big-eye.png'
 import logo from '../public/logo.jpg'
 import surgery from '../public/surgery.jpg'
 import clinic from '../public/clinic.jpg'
-import map from '../public/map.png'
+import map1 from '../public/map-aflitos.png'
+import map2 from '../public/map-pina.png'
 
 export default function Home() {
+  const addresses = [
+    {
+      street: 'Avenida Rosa e Silva 428',
+      neighborhood: 'Aflitos',
+      cityState: 'Recife - PE',
+      map: map1,
+    },
+    {
+      street: 'Avenida Conselheiro Aguiar 726',
+      neighborhood: 'Boa Viagem',
+      cityState: 'Recife - PE',
+      map: map2,
+    },
+  ]
+  
+  const [selectedAddress, setSelectedAddress] = useState(addresses[0])
+
+  const checkIsActive = (address) => {
+    return selectedAddress.street === address.street ? styles.locationActive : ''
+  }
+
   return (
     <div>
       <Head>
@@ -129,15 +152,22 @@ export default function Home() {
           </div>
         </section>
         <section id="aboutLocation" className={styles.aboutLocation}>
-          <h2>Clínica</h2>
-          <div className={styles.locationImagesContainer}>
-            <div className={`${styles.locationContainer} ${styles.locationMap}`}>
-              <Image className={styles.photo} src={map} alt="Location"/>
+          <h2>Clínicas Parceiras</h2>
+          <div className={styles.locationMapContainer}>
+            <div className={styles.locationMap}>
+              <Image className={styles.photo} src={selectedAddress.map} alt="Location"/>
             </div>
-            <div className={styles.locationContainer}>
-              <p>Equipe médica com mais de <br/>35 anos de experiência.</p>
-              <Image className={styles.photo} src={clinic} width={280} height={180} alt="Medical Clinic"/>
+            <div className={styles.locationAddresses}>
+              <p className={checkIsActive(addresses[0])} onClick={() => setSelectedAddress(addresses[0])}>
+                {addresses[0].street}<br/>{addresses[0].neighborhood}, {addresses[0].cityState}
+              </p>
+              <p className={checkIsActive(addresses[1])} onClick={() => setSelectedAddress(addresses[1])}>
+               {addresses[1].street}<br/>{addresses[1].neighborhood}, {addresses[1].cityState}
+              </p>
             </div>
+          </div>
+          <div className={styles.locationImagesGalery}>
+            <Image className={styles.photo} src={clinic} alt="Medical Clinic"/>
           </div>
         </section>
         <section className={styles.contact}>
