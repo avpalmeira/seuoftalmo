caa0f8c49d19310d14bde1637e4b9b612d21b2e9
Antonio Palmeira <av.palmeira@gmail.com>
2022-03-12 15:28:23 -0300

    Use GTM scripts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,8 +8,16 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap" rel="stylesheet" />
+          {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+          <script id="gtm-tracker" dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PSCGSVP')`}}></script>
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSCGSVP"
+          height="0" width="0" style="display:none,visibility:hidden"}}></iframe>`}}></noscript>
           <Main />
           <NextScript />
         </body>
